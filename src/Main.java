4165eaad1ac48999316ffa1f458b11f163d18f5e
Markus23D <maan0007@stud.kea.dk>
2023-09-11 15:08:07 +0200

    Tilføjet konstruktør

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        Superhelt helt1 = new Superhelt();
+
+    }
 }
--- src/Superhelt.java
+++ src/Superhelt.java
@@ -1,2 +1,26 @@
 public class Superhelt {
+    private String heroName;
+    private String realName;
+    private String superPower;
+    private int yearCreated;
+    private String isHuman;
+    private double strenght;
+
+    //Konstruktør
+    public Superhelt (String heroName, String realName,String superPower, int yearCreated, String isHuman, double strenght){
+        this.heroName = heroName;
+        this.realName=realName;
+        this.superPower=superPower;
+        this.yearCreated=yearCreated;
+        this.isHuman=isHuman;
+        this.strenght=strenght;
+    }
+
+    //get metode
+    public String
+
+    //set metode
+
+
+
 }
