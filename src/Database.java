78171533f7a3566430b990fc7d1745855206526c
Markus23D <maan0007@stud.kea.dk>
2023-09-12 12:29:55 +0200

    Alle spørgesmål om superhelte er lavet

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,19 @@
+
+
 public class Database {
+
+    private Superhelt[] superHelteListe;
+    int count = 0;
+
+    public Database() {
+        this.superHelteListe= new Superhelt[5];
+    }
+    public void tilføjSuperHelte(String heroName, String realName, String superPower, int yearCreated, String isHuman, double strenght){
+        superHelteListe[count ++] = new Superhelt(heroName, realName, superPower, yearCreated, isHuman, strenght);
+
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
+import javax.xml.crypto.Data;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
 
-        Superhelt helt1 = new Superhelt();
+    public static void main(String[] args) {
+        System.out.println("Velkommen til SUPERHERO UNIVERSET");
+
+        Scanner keyboard = new Scanner(System.in);
+        //lav DataBase
+        Database liste = new Database();
+
+
+        System.out.println("Hvad er heltens superhelte navn?");
+        String superHeroName = keyboard.nextLine();
+
+        System.out.println("Hvad er heltens rigtige navn?");
+        String realName = keyboard.nextLine();
+
+
+        System.out.println("Hvad er heltens superkræft");
+        String superPower = keyboard.nextLine();
+
+        System.out.println("Hvilket år er superhelten lavet");
+        int yearCreated = Integer.valueOf(keyboard.nextLine());
+
+        System.out.println("Er superhelten menneske?");
+        String isHuman = keyboard.nextLine();
+
+        System.out.println("Hvad er superheltens styrke");
+        double strenght = keyboard.nextDouble();
+
+
+        liste.tilføjSuperHelte(superHeroName, realName, superPower, yearCreated , isHuman, strenght );
+
+
+        System.out.println();
+
+
 
     }
 }
