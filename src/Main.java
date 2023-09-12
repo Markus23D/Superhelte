ef9104406b8bacfa4c1b34111f7ddb35878d5fa6
Markus23D <maan0007@stud.kea.dk>
2023-09-12 14:51:59 +0200

    lavet en start menu og når du er hvor du kan vælge mellem at alve en superhelt eller afslutte

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,58 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Velkommen til SUPERHERO UNIVERSET");
-
         Scanner keyboard = new Scanner(System.in);
         //lav DataBase
         Database liste = new Database();
+        //Intro
+        System.out.println("Velkommen til SUPERHERO UNIVERSET");
+        System.out.println("\u2500".repeat(50));
+        System.out.println(" ");
+        //startmenu
 
 
-        System.out.println("Hvad er heltens superhelte navn?");
-        String superHeroName = keyboard.nextLine();
+        while (true) {
+            System.out.println("Indtast 1 for at oprette en superhelt");
+            System.out.println("Indtast 9 for at afslutte");
+            int userChoice = Integer.valueOf(keyboard.nextLine());
 
-        System.out.println("Hvad er heltens rigtige navn?");
-        String realName = keyboard.nextLine();
+            if (userChoice == 1) {
+                System.out.println("Okay, lad os gøre det!");
+                System.out.println("Hvad er heltens superhelte navn?");
+                String superHeroName = keyboard.nextLine();
 
-
-        System.out.println("Hvad er heltens superkræft");
-        String superPower = keyboard.nextLine();
-
-        System.out.println("Hvilket år er superhelten lavet");
-        int yearCreated = Integer.valueOf(keyboard.nextLine());
-
-        System.out.println("Er superhelten menneske?");
-        String isHuman = keyboard.nextLine();
-
-        System.out.println("Hvad er superheltens styrke");
-        double strenght = keyboard.nextDouble();
+                System.out.println("Hvad er heltens rigtige navn?");
+                String realName = keyboard.nextLine();
 
 
-        liste.tilføjSuperHelte(superHeroName, realName, superPower, yearCreated , isHuman, strenght );
+                System.out.println("Hvad er heltens superkræft");
+                String superPower = keyboard.nextLine();
+
+                System.out.println("Hvilket år er superhelten lavet");
+                int yearCreated = Integer.valueOf(keyboard.nextLine());
+
+                System.out.println("Er superhelten menneske? [Yes/no]");
+                String isHuman = keyboard.nextLine();
+
+                System.out.println("Hvad er superheltens styrke lvl");
+                double strenght = Double.valueOf(keyboard.nextLine());
 
 
-        System.out.println();
+                liste.tilføjSuperHelte(superHeroName, realName, superPower, yearCreated, isHuman, strenght);
+
+                System.out.println("Superhelt tilføjet til Database");
+                System.out.println("\u2500". repeat(50));
+                System.out.println(" ");
 
 
+            } else if (userChoice == 9) {
+                System.exit(0);
+            } else {
+                System.out.println("Ugyldigt valg. Prøv igen");
+            }
 
+        }
     }
+
+
 }
